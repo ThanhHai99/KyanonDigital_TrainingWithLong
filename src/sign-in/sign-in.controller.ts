db74a55c962186955bb2ba3d4b6093939be78961
ThanhHai99 <haitran23031999@gmail.com>
2021-07-06 15:24:50 +0700

    Complete check JWT

--- src/sign-in/sign-in.controller.ts
+++ src/sign-in/sign-in.controller.ts
@@ -1,27 +1,47 @@
-import { Controller, Get, Post, Param, Body, Delete, Query } from '@nestjs/common';
+import { Controller, Post, Body, Response } from '@nestjs/common';
+import * as jwt from "jsonwebtoken";
 import { CreateUserDto } from './../users/create-user.dto';
 import { User } from './../users/users.entity';
 import { SignInService } from './sign-in.service';
+require("dotenv").config();
 
 @Controller('signIn')
 export class SignInController {
     constructor(private signInService: SignInService) {};
 
     @Post()
-    async index(@Body() createUserDto: CreateUserDto) {
-        let user = new User();
-        user.username = createUserDto.username;
-        user.password = createUserDto.password;
-        user.name = createUserDto.name;
-        
-        try {
-            const users = await this.signInService.isExist(user);
-            if (!users.checkIfUnencryptedPasswordIsValid(user.password)) {
-                return "Invalid username or password";
-            };
-            return "Sign in successfully";
-        } catch (error) {
-            return "Error";
+    async index(@Body() createUserDto: CreateUserDto, @Response() res) {
+        let user0 = new User();
+        user0.username = createUserDto.username;
+        user0.password = createUserDto.password;
+
+        // Check if username and password are set
+        if (!(user0.username && user0.password)) {
+            return res.status(400).send();
         };
+        
+        let user: User;
+        try {
+            // Get user from signIn service
+            user = await this.signInService.isExist(user0);
+        } catch (error) {
+            return res.status(401).send();
+        };
+        
+        // Check if encrypted password match
+        if (!user.checkIfUnencryptedPasswordIsValid(user0.password)) {
+            return res.status(401).send();
+        };
+
+        // Sing jwt
+        const token = jwt.sign(
+            { userId: user.id, username: user.username },
+            process.env.jwtSecret,
+            { expiresIn: process.env.tokenLifetime }
+        );
+
+        // Send the jwt in the response
+        res.setHeader("auth", token);
+        return res.status(200).send("Sign in successfully");
     };
 };
