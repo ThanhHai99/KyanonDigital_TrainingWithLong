f798fb8fe451880e32df2dc3383fdd7e53998738
ThanhHai99 <haitran23031999@gmail.com>
2021-08-30 18:49:26 +0700

    [Update] feature-price_log

---
+++ src/modules/price_log/service/price_logs.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PriceLog } from 'src/modules/price_log/entity/price_logs.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class PriceLogService {
+    constructor(
+        @InjectRepository(PriceLog)
+        private priceLogRepository: Repository<PriceLog>
+    ) {}
+
+    async getAll(): Promise<PriceLog[]> {
+        return await this.priceLogRepository.find();
+    }
+
+    async getById(id: number): Promise<PriceLog> {
+        return await this.priceLogRepository.findOne(id);
+    }
+
+    async create(categoryLog: PriceLog): Promise<PriceLog> {
+        return await this.priceLogRepository.save(categoryLog);
+    }
+}
